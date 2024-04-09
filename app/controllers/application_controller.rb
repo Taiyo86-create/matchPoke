f95fc6290a9e54547dbd161ab31621b7ecbc0431
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-04-09 14:47:50 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+  before_action :basic_auth
 
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :age_id, :sex_id, :status_id, :favorite_title_id, :profile, :phone_number, :icon_id])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'Taiyo' && password == 'hogeFuga'
+    end
   end
 end
