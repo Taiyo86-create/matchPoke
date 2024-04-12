fcfa18690b54b1c7d955e9deb3d8cb654f4cc1c1
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-04-12 17:14:44 +0900

    非同期通信でデータを遅れるよう編集

--- app/javascript/message.js
+++ app/javascript/message.js
@@ -2,8 +2,10 @@ function post (){
     const form = document.getElementById("form");
     form.addEventListener("submit", () => {
         const formData = new FormData(form);
+        const user_id = document.getElementById("user_id").value; // ユーザーIDを取得
+        const match_id = document.getElementById("match_id").value; // マッチIDを取得
         const XHR = new XMLHttpRequest();
-        XHR.open("POST", "/users/:user_id/matches/:match_id/messages", true);
+        XHR.open("POST", `/users/${user_id}/matches/${match_id}/messages`, true);
         XHR.responseType = "json";
         XHR.send(formData);
     });
