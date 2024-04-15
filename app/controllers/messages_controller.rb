ae87f4f94f8a24e9cb417e4e170c10deeb320883
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-04-15 14:51:50 +0900

    バグがあったので非同期か解除

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,7 +10,7 @@ class MessagesController < ApplicationController
     @match = Match.find(params[:match_id])
     @message = @match.messages.new(message_params)
     if @message.save
-      render json:{ post: @message }
+      redirect_to new_user_match_messages_path(id: current_user.id, user_id: current_user.id, match_id: @match.id)
     else
       @messages = @match.messages.includes(:user)
       render :new
